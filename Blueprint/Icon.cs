205606454e347d680fa3e0631de901a39de6df12
Prokop Randáček <prokop@randacek.dev>
2020-12-22 15:20:50 +0100

    Icon class

---
+++ Blueprint/Icon.cs
@@ -0,0 +1,15 @@
+namespace Blueprint
+{
+    public class Icon
+    {
+        /// <summary>
+        /// Index of the icon, 1-based. 
+        /// </summary>
+        public int Index;
+
+        /// <summary>
+        /// The icon that is displayed.
+        /// </summary>
+        public Signal Signal;
+    }
+}
