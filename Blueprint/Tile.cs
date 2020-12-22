6a4f7a245e3249c87eb19ac39ec4cb92c3233ac1
Prokop Randáček <prokop@randacek.dev>
2020-12-22 15:21:50 +0100

    Tile class

---
+++ Blueprint/Tile.cs
@@ -0,0 +1,15 @@
+namespace Blueprint
+{
+    public class Tile
+    {
+        /// <summary>
+        /// Prototype name of the tile (e.g. "concrete")
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Position of the entity within the blueprint.
+        /// </summary>
+        public Position Position;
+    }
+}
