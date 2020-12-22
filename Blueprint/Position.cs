2178adc9d614a64c99a522fa4615508ae0061879
Prokop Randáček <prokop@randacek.dev>
2020-12-22 15:21:37 +0100

    Position class

---
+++ Blueprint/Position.cs
@@ -0,0 +1,15 @@
+namespace Blueprint
+{
+    public class Position
+    {
+        /// <summary>
+        /// X position within the blueprint, 0 is the center.
+        /// </summary>
+        public float X;
+        
+        /// <summary>
+        /// Y position within the blueprint, 0 is the center. 
+        /// </summary>
+        public float Y;
+    }
+}
